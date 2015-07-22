232c22b26dfbc6b7160732bd3b6daa452e96aecb
Jon Roig <jon@jonroig.com>
2015-07-22 12:53:33 -0700

    config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports.settings = {
     tokenUrl: 'https://login.microsoftonline.com/7e6178cf-6e84-42ca-9206-5779585ec237/oauth2/token',
     clientId: '7d2eeb09-ffdf-4862-84c3-0c0db0d8a3aa',
     clientSecret: '21X3JKdZClpPKh6m80BgNcfyjF98D5zpzsanhfPX2NI=',
-    callbackUrl: 'url_for_your_app_called_by_azure_upon_completing_authentication',
+    callbackUrl: 'http://officecommander.azurewebsites.net/sucess',
     resource: 'https://graph.windows.net',
     mongodb: 'mongodb://OfficeCommanderDB:H9o_giWsudThHfX5WFum4XZXixeYB2WT2SPALxQia2Y-@ds036698.mongolab.com:36698/OfficeCommanderDB'
 };
