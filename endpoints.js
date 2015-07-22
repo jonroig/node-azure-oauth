a479ea9cb47b17eb599cb3e568e18de59d168b02
Jon Roig <jon@jonroig.com>
2015-07-22 16:51:22 -0700

    blah

--- endpoints.js
+++ endpoints.js
@@ -44,7 +44,9 @@ module.exports.configureEndpoints = function (app, passport) {
         console.log('req.user', req.user);
         require('request').get(
             'https://graph.microsoft.com/beta/me/Messages?$orderby=' + encodeURIComponent('DateTimeReceived desc'),
-            {},
+            {
+                Authorization: 'bearer: ' + req.user.sub
+            },
             function (error, response, body) {
                 if (error) {
                     console.log('error', error);
