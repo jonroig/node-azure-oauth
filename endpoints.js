e48d889478037d5c19fca67eb2f019a3ef3a6cd8
Jon Roig <jon@jonroig.com>
2015-07-22 17:00:17 -0700

    blah

--- endpoints.js
+++ endpoints.js
@@ -28,7 +28,7 @@ module.exports.configureEndpoints = function (app, passport) {
     app.get('/auth/waad', passport.authenticate('oauth2'));
 
     // callback from Azure to complete auth sequence
-    app.get('/auth/waad/callback', passport.authenticate('oauth2', { failureRedirect: '/login' }),
+    app.get('/auth/waad/callback', q.authenticate('oauth2', { failureRedirect: '/login' }),
       function (req, res) {
           res.redirect('/');
       });
@@ -40,25 +40,27 @@ module.exports.configureEndpoints = function (app, passport) {
     });
 
     app.get('/mail', ensureAuthenticated, function (req, res) {
+
+        console.log('TOKEN', passport.user.getToken('https://outlook.office365.com'));
         // console.log('req', req);
-        console.log('req.user', req.user);
-        require('request').get(
-            'https://graph.microsoft.com/beta/me/Messages?$orderby=' + encodeURIComponent('DateTimeReceived desc'),
-            {'auth': {
-                bearer: req.user.sub
-              }
-            },
-            function (error, response, body) {
-                if (error) {
-                    console.log('error', error);
-                    next(error);
-                }
-                else {
-                    console.log('body', body);
-                    // data = { user: passport.user, msgs: JSON.parse(body)['value'] };
-                    // res.render('mail', { data: data });
-                }
-            }
-        );
+        // console.log('req.user', req.user);
+        // require('request').get(
+        //     'https://graph.microsoft.com/beta/me/Messages?$orderby=' + encodeURIComponent('DateTimeReceived desc'),
+        //     {'auth': {
+        //         bearer: req.user.sub
+        //       }
+        //     },
+        //     function (error, response, body) {
+        //         if (error) {
+        //             console.log('error', error);
+        //             next(error);
+        //         }
+        //         else {
+        //             console.log('body', body);
+        //             // data = { user: passport.user, msgs: JSON.parse(body)['value'] };
+        //             // res.render('mail', { data: data });
+        //         }
+        //     }
+        // );
     });
 };
